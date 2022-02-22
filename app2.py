15cf4cd37a3ab6ade6596cbcf39032581557baeb
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-02-22 22:41:14 +0000

    orario finito

--- app2.py
+++ app2.py
@@ -1,13 +1,27 @@
-#realizzare un server web che visualizzi l'ora e colori lo sfondo in base all'orario: un colore per la mattina, uno per il pomeriggio ed un per la notte
+#realizzare un server web che visualizzi l'ora e colori lo sfondo in base all'orario: un colore per la mattina, uno per il pomeriggio, uno per la sera ed un per la notte
 from flask import Flask, render_template
 app = Flask(__name__)
 
-from datetime import datetime
-tempo = datetime.time
+import datetime
+ora = datetime.datetime.now()
+ora = ora.hour
 
-@app.route('/', methods=['GET'])
-def ciao_mondo():
-    return (tempo)
+if ora >= 6 and ora < 13:
+  @app.route('/', methods=['GET'])
+  def mattina():
+    return render_template("mattina.html")
+elif ora >= 18 and ora < 22:
+  @app.route('/', methods=['GET'])
+  def pomeriggio():
+    return render_template("pomeriggio.html")    
+elif ora >= 18 and ora < 22:
+  @app.route('/', methods=['GET'])
+  def sera():
+    return render_template("sera.html")
+else:
+  @app.route('/', methods=['GET'])
+  def notte():
+      return render_template("notte.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
