d98fc9b6246e2a4e6cc153ce853b4a8975f027b6
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-02-23 20:19:36 +0000

    prova

--- esercizio1.py
+++ esercizio1.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def immagini():
     return render_template("immagini.html")
 
-
+@app.route('/meteo', methods=['GET'])
+def meteo():
+  nran = random.randint(0,8)
+  if nran <= 2:
+        immagine = "static/images/pioggia.jpg"
+  elif nran <= 5:
+        immagine = "static/images/nuvoloso.jpg"
+  else:
+        immagine = "static/images/sole.jpg"
+  return render_template("previsioni.html", meteo = immagine)
 
 
 if __name__ == '__main__':
