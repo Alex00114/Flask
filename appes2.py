1a55c2bd5f6518957b3fda81afe2b4491098cd3c
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-03-09 13:20:07 +0000

    finito esercizio registrazione/login

--- appes2.py
+++ appes2.py
@@ -3,11 +3,12 @@
 #Prevedere la possibilità di fare il login inserendo username e password. Se sono corrette fornire un messaggio di benvenuto diverso a seconda del sesso.
 from flask import Flask, render_template, request
 app = Flask(__name__)
+lista = []
 
 
 @app.route('/', methods=['GET'])
 def home_page():
-    return render_template("login_appes2.html")
+    return render_template("registrazione.html")
 
 @app.route('/data', methods=['GET'])
 def dati():
@@ -17,17 +18,24 @@ def dati():
     password = request.args["Password"]
     sesso = request.args["Sex"]
     if password == conferma:
-        if nome == "" or password == "" or conferma == "" or username == "":
-            return '<h1>Non tutti i campi sono stati specificati</h1>'
-        else:
-            if sesso == "M":
-                benv = "Benvenuto"
-                return render_template("welcome.html", name = nome, benvenuto = benv)
-            else:
-                benv = "Benvenuta"
-                return render_template("welcome.html", name = nome, benvenuto = benv)
+        lista.append({'name':nome,'username':username,'password':password,'sex':sesso})
+        return render_template("login_appes2.html", name = nome)
     else:
         return '<h1>Errore, password non coincidenti</h1>'
+
+@app.route('/login', methods=['GET'])
+def login():
+    username_log = request.args["Username"]
+    password_log = request.args["Password"]
+    for utente in lista:
+        if utente["username"] == username_log and utente["password"] == password_log:
+            if utente["sex"] == "M":
+                benv = "Benvenuto"
+                return render_template("welcome.html", name = utente["name"], benvenuto = benv)
+            else:
+                benv = "Benvenuta"
+                return render_template("welcome.html", name = utente["name"], benvenuto = benv)
+    return '<h1><b>Errore, username o password non corretti</b></h1>'
 
 
 if __name__ == '__main__':
