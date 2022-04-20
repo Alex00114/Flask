a72461602e0fd8cf79ddbf877e09295d5a406669
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-04-20 13:31:51 +0000

    .

--- correzioneVerificaA_2.1/correzione_VerificaA_2.1.py
+++ correzioneVerificaA_2.1/correzione_VerificaA_2.1.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
+from flask import Flask, render_template, request, send_file, make_response, url_for, Response, redirect
 app = Flask(__name__)
-import pandas as pd
 import io
-import geopandas as gpd
+import geopandas
 import contextily
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import pandas as pd
 
 comuni = geopandas.read_file("/workspace/Flask/correzioneVerificaA_2.1/Com01012021_g_WGS84.zip")
 provincie = geopandas.read_file("/workspace/Flask/correzioneVerificaA_2.1/ProvCM01012021_g_WGS84.zip")
@@ -17,7 +17,7 @@ ripartizioni = geopandas.read_file("/workspace/Flask/correzioneVerificaA_2.1/tem
 
 
 @app.route('/', methods=['GET'])
-def Home():
+def home():
     return render_template("home.html")
 
 @app.route('/input', methods=['GET'])
@@ -40,14 +40,16 @@ def mappa():
     fig, ax = plt.subplots(figsize = (12,8))
 
     mappa_prov.to_crs(epsg=3857).plot(ax=ax, alpha=0.2, edgecolor = "k")
-    com_prov.to_crs(epsg=3857).plot(ax=ax, alpha=0.4, edgecolor = "r")
+    comuni_prov.to_crs(epsg=3857).plot(ax=ax, alpha=0.4, edgecolor = "r")
     contextily.add_basemap(ax=ax)
 
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-
+@app.route('/dropdownReg', methods=['GET'])
+def dropReg():
+    return render_template("dropdownReg.html", regioni2 = regioni["DEN_REG"].sort_values(ascending = True))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
